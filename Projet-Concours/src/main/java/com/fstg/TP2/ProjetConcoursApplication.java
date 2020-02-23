82bc51c8a22de1314844a355ac1973dc6bf9c02b
HP <HP@DESKTOP-3IGV47K>
2020-02-23 12:38:52 +0100

    First push by Nassima

---
+++ Projet-Concours/src/main/java/com/fstg/TP2/ProjetConcoursApplication.java
@@ -0,0 +1,13 @@
+package com.fstg.TP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetConcoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetConcoursApplication.class, args);
+	}
+
+}
