7007b72d487fd5044062b17b6209ba6e0497c693
HP <HP@DESKTOP-3IGV47K>
2020-02-23 13:03:24 +0100

    Departement

---
+++ Projet-Concours/src/main/java/com/fstg/TP2/bean/Departement.java
@@ -0,0 +1,65 @@
+package com.fstg.TP2.bean;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+public class Departement {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nom;
+	private String description;
+	@OneToMany(mappedBy = "departement")
+	private List<Filiere> filieres;
+
+	public Departement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Departement(Long id, String nom, String description, List<Filiere> filieres) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.description = description;
+		this.filieres = filieres;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Filiere> getFilieres() {
+		return filieres;
+	}
+
+	public void setFilieres(List<Filiere> filieres) {
+		this.filieres = filieres;
+	}
+
+}
