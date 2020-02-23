e93cf2a843e57c3570dee4f923631b79c1981a2a
HP <HP@DESKTOP-3IGV47K>
2020-02-23 12:57:48 +0100

    third push By Nassima

---
+++ Projet-Concours/src/main/java/com/fstg/TP2/bean/ConfigConcours.java
@@ -0,0 +1,57 @@
+package com.fstg.TP2.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ConfigConcours {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@ManyToOne
+	private Concours concours;
+	@ManyToOne
+	private Module module;
+	private Double coeffecient;
+
+	public Double getCoeffecient() {
+		return coeffecient;
+	}
+
+	public void setCoeffecient(Double coeffecient) {
+		this.coeffecient = coeffecient;
+	}
+
+	public ConfigConcours() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Concours getConcours() {
+		return concours;
+	}
+
+	public void setConcours(Concours concours) {
+		this.concours = concours;
+	}
+
+	public Module getModule() {
+		return module;
+	}
+
+	public void setModule(Module module) {
+		this.module = module;
+	}
+
+}
