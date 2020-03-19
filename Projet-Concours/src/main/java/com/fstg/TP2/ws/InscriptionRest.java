13d498414dbe03ad5cb7e3ae50c61585b1833d97
HP <HP@DESKTOP-3IGV47K>
2020-03-19 16:15:44 +0100

    Nassima

--- Projet-Concours/src/main/java/com/fstg/TP2/ws/InscriptionRest.java
+++ Projet-Concours/src/main/java/com/fstg/TP2/ws/InscriptionRest.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,7 @@ public class InscriptionRest {
 		return incsriptionService.deleteByEtudiantCne(cne);
 	}
 
+	@PostMapping("/")
 	public int save(@RequestBody Etudiant etudiant, List<Inscription> inscriptions) {
 		return incsriptionService.save(etudiant, inscriptions);
 	}
