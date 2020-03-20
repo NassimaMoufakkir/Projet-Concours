7de44d30fd1708e45074d3ffaa4ba003a98c6f5f
PC <PC@192.168.1.9>
2020-03-20 15:56:04 +0100

    modifications

--- Projet-Concours/src/main/java/com/fstg/TP2/service/facade/ConfigConcoursService.java
+++ Projet-Concours/src/main/java/com/fstg/TP2/service/facade/ConfigConcoursService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.fstg.TP2.bean.Concours;
 import com.fstg.TP2.bean.ConfigConcours;
 
+//try
+
 public interface ConfigConcoursService {
 	public List<ConfigConcours> findByConcoursReference(String reference);
 	public List<ConfigConcours> findByTypeDiplomeLibelle(String libelle);
